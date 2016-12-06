abf70f6ae62324c4fae9a85b52bd5e1e3feedd77
Lindsey Adams <linzma87@gmail.com>
2016-12-06 18:26:07 -0500

    template setup

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,31 @@
 (function($) {
   "use strict";
 
+  var portfolioApp = angular.module('portfolioApp', ['ui.router']);
+
+  portfolioApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('PortfolioParent', {
+          url: '/',
+          abstract: true,
+          template: '<ui-view></ui-view>'
+      }).state('PortfolioParent.about', {
+          url: '/about',
+          // controller: 'HomeController as home',
+          templateUrl: 'src/templates/about.html'
+      }).state('PortfolioParent.contact', {
+          url: '/contact',
+          // controller: 'AllProjectsController as allProjects',
+          templateUrl: 'src/templates/contact.html'
+      }).state('PortfolioParent.work', {
+          url: '/work',
+          // controller: 'ClientDashboardController as clientDashboard',
+          templateUrl: 'src/templates/work.html'
+      });
+
+    });
+
+
 })(jQuery);
