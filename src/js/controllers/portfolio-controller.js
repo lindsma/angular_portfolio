2fe625dab6d58d9fff8f215830efd436a7e4ecda
Lindsey Adams <linzma87@gmail.com>
2016-12-13 23:33:31 -0500

    responsive homepage

--- src/js/controllers/portfolio-controller.js
+++ src/js/controllers/portfolio-controller.js
@@ -1,36 +1,9 @@
 angular.module('portfolioApp').controller('PortfolioController', function($scope, $state, $location) {
 
-// function checkUrl() {
-//   if ($location.$$url === '/home') {
-//     this.home = true;
-//   } else if ($location.$$url !== '/home'){
-//     this.home = false;
-//   }
-// }
+$scope.showMenu = false;
 
-$scope.toggleActive = function(page) {
-  if (page == 'about') {
-    $scope.about = true;
-    $scope.contact = false;
-    $scope.work = false;
-  } else if (page == 'contact') {
-    $scope.about = false;
-    $scope.contact = true;
-    $scope.work = false;
-  } else if (page == 'work') {
-    $scope.about = false;
-    $scope.contact = false;
-    $scope.work = true;
-  } else {
-    $scope.about = false;
-    $scope.contact = false;
-    $scope.work = false;
-  }
+$scope.toggleMenu = function() {
+  $scope.showMenu = !$scope.showMenu;
 };
-
-
-
-// checkUrl();
-
 
 });
